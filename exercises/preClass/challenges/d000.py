b31707416c714d6be148c44d1c0f22822818e2d2
Rafael Gomes <156968191+rafa-rx580@users.noreply.github.com>
2025-04-11 14:16:31 -0300

    back to beginning

---
+++ exercises/preClass/challenges/d000.py
@@ -0,0 +1,2 @@
+name = input('Qual é o seu nome?')
+print ('Prazer te conhecer, ', name, '!')
---
+++ exercises/preClass/ex/ex000.py
@@ -0,0 +1,4 @@
+name = input('Qual é o seu nome? ')
+idade = input('Qual é sua idade? ')
+peso = input('Qual é o seu peso? ')
+print(name,idade,peso)
