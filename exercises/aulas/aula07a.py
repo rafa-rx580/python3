b0d1ece8ff407cf92f06926b27ce118db66fc68c
Rafael Gomes <156968191+rafa-rx580@users.noreply.github.com>
2025-06-27 17:17:24 -0300

    aula07

---
+++ exercises/aulas/aula07a.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+print("{} + {} = {}" .format(n1, n2, s) )
+print("{} * {} = {}" .format(n1, n2, m))
+print("{} / {} = {}" .format(n1, n2, d))
